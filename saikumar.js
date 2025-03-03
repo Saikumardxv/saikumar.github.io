5c3c80a29e7f95a525ccd466d126f7fcdb722861
Saikumardxv <gudipatisaikumar46@gmail.com>
2025-03-03 20:53:24 +0530

    Add files via upload

---
+++ saikumar.js
@@ -0,0 +1,6 @@
+console.log("Welcome to My Webpage!");
+
+// Add event listener to header
+document.querySelector("header").addEventListener("click", () => {
+    console.log("Header clicked!");
+});
